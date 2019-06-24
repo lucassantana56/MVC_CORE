edce9d5632628515782939aab7e4863f1f0207e8
Lucas Santana <lucas.santana@xpand-it.com>
2019-06-24 14:15:39 +0100

    eventfeed

--- PAP.Web/DevCommunity2.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PAP.Web/DevCommunity2.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -57,7 +57,7 @@ namespace DevCommunity2.Web.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Feed/Index");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
